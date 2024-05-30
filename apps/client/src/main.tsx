d4ac6c65e46bc2981f70a47ad8a60e92ab4255e2
Laurent Chau <mplaurent.chau@gmail.com>
2024-05-30 14:30:23 +0200

    fix(router): fix tanstack router implementation

--- apps/client/src/main.tsx
+++ apps/client/src/main.tsx
@@ -1,37 +1,23 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { RouterProvider, createRouter } from "@tanstack/react-router";
-import { StrictMode, Suspense, lazy } from "react";
+import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 
 import "~/assets/css/globals.css";
-import { ThemeProvider } from "~/components/theme-provider";
-import { Toaster } from "~/components/ui/sonner";
 import { routeTree } from "~/routeTree.gen";
 
 const queryClient = new QueryClient();
+
+export type Context = {
+  queryClient: QueryClient;
+};
+
 const router = createRouter({
   context: { queryClient },
   defaultPreload: "intent",
   defaultPreloadStaleTime: 0,
   routeTree,
 });
-
-const ReactQueryDevtools =
-  process.env.NODE_ENV === "production"
-    ? () => null
-    : lazy(() =>
-        import("@tanstack/react-query-devtools").then((res) => ({
-          default: res.ReactQueryDevtools,
-        })),
-      );
-const TanStackRouterDevtools =
-  process.env.NODE_ENV === "production"
-    ? () => null
-    : lazy(() =>
-        import("@tanstack/router-devtools").then((res) => ({
-          default: res.TanStackRouterDevtools,
-        })),
-      );
 
 declare module "@tanstack/react-router" {
   interface Register {
@@ -47,16 +33,7 @@ if (rootElement && !rootElement.innerHTML) {
   root.render(
     <StrictMode>
       <QueryClientProvider client={queryClient}>
-        <ThemeProvider defaultTheme="light" storageKey="vite-ui-theme">
-          <RouterProvider router={router} />
-          <Toaster richColors />
-        </ThemeProvider>
-        <Suspense>
-          <ReactQueryDevtools />
-        </Suspense>
-        <Suspense>
-          <TanStackRouterDevtools />
-        </Suspense>
+        <RouterProvider router={router} />
       </QueryClientProvider>
     </StrictMode>,
   );
--- apps/client/src/routes/__root.tsx
+++ apps/client/src/routes/__root.tsx
@@ -1,5 +1,37 @@
 import { Outlet, createRootRoute } from "@tanstack/react-router";
+import { Suspense, lazy } from "react";
+
+import { ThemeProvider } from "~/components/theme-provider";
+import { Toaster } from "~/components/ui/sonner";
+
+const ReactQueryDevtools =
+  process.env.NODE_ENV === "production"
+    ? () => null
+    : lazy(() =>
+        import("@tanstack/react-query-devtools").then((res) => ({
+          default: res.ReactQueryDevtools,
+        })),
+      );
+const TanStackRouterDevtools =
+  process.env.NODE_ENV === "production"
+    ? () => null
+    : lazy(() =>
+        import("@tanstack/router-devtools").then((res) => ({
+          default: res.TanStackRouterDevtools,
+        })),
+      );
 
 export const Route = createRootRoute({
-  component: () => <Outlet />,
+  component: () => (
+    <ThemeProvider defaultTheme="light" storageKey="vite-ui-theme">
+      <Outlet />
+      <Toaster richColors />
+      <Suspense>
+        <ReactQueryDevtools />
+      </Suspense>
+      <Suspense>
+        <TanStackRouterDevtools />
+      </Suspense>
+    </ThemeProvider>
+  ),
 });
